be3ddc0e03aa731710c2149be4bd7c8ef5e39ed8
DeepDhrangadhariya <deepgajjar008@gmail.com>
2025-02-01 00:27:34 +0530

    complete single product page UI, install redux, create cart slice and store, complete show cart modal, some cart logics for quantity, remove product, clear cart

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react"
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import CartModal from "../pages/shop/CartModal"
 
 
 const Navbar = () => {
+
+    const products = useSelector((state) => state.cart.products)
+    // console.log(products)
+    const [isCartOpen, setIsCartOpen] = useState(false)
+
+    const handleCartToggle = () => {
+        setIsCartOpen(!isCartOpen)
+    }
+
     return (
         <header className="fixed-nav-bar w-nav">
             <nav className="max-w-screen-2xl mx-auto px-4 flex justify-between items-center">
@@ -27,9 +39,9 @@ const Navbar = () => {
                         </Link>
                     </span>
                     <span>
-                        <button className="hover:text-primary">
+                        <button onClick={handleCartToggle} className="hover:text-primary">
                             <i className="ri-shopping-bag-line"></i>
-                            <sup className="text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center">0</sup>
+                            <sup className="text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center">{products.length}</sup>
                         </button>
                     </span>
                     <span>
@@ -39,6 +51,10 @@ const Navbar = () => {
                     </span>
                 </div>
             </nav>
+
+            {
+                isCartOpen && <CartModal products={products} isOpen={isCartOpen} onClose={handleCartToggle} />
+            }
         </header>
     )
 }
--- frontend/src/pages/shop/ProductCards.jsx
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
 import RatingsStars from "../../components/RatingsStars";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/features/cart/cartSlice";
 
 
 const ProductCards = ({ products }) => {
 
     // console.log(products);
+
+    const dispatch = useDispatch()
+
+    const handleAddToCart = (product) => {
+        dispatch(addToCart(product))
+    }
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
@@ -12,12 +20,17 @@ const ProductCards = ({ products }) => {
                 products.map((product, index) => (
                     <div className="product__card" key={index}>
                         <div className="relative">
-                            <Link to={`/shop/${product._id}`}>
+                            <Link to={`/shop/${product._id || product.id}`}>
                                 <img src={product.image} alt="product iamge" className="max-h-96 md:h-64 w-full object-cover hover:scale-105 transition-all duration-300" />
                             </Link>
 
                             <div className="hover:block absolute top-3 right-3">
-                                <button><i className="ri-shopping-cart-2-line bg-primary p-1.5 text-white hover:bg-primary-dark"></i></button>
+                                <button
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        handleAddToCart(product)
+                                }}
+                                ><i className="ri-shopping-cart-2-line bg-primary p-1.5 text-white hover:bg-primary-dark"></i></button>
                             </div>
                         </div>
 
