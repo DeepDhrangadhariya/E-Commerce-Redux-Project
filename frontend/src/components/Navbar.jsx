e26daae69800c0007077b044ca1a974926bc58a6
DeepDhrangadhariya <deepgajjar008@gmail.com>
2025-02-12 16:40:17 +0530

    navbar dropdown menus

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
-import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
 import CartModal from "../pages/shop/CartModal"
+import avatarImg from '../assets/avatar.png'
+import { useLogoutUserMutation } from "../redux/features/auth/authAPI"
+import { logout } from "../redux/features/auth/authSlice"
 
 
 const Navbar = () => {
@@ -13,6 +16,49 @@ const Navbar = () => {
     const handleCartToggle = () => {
         setIsCartOpen(!isCartOpen)
     }
+
+    // show user if logged in
+    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.auth)
+    const [logoutUser] = useLogoutUserMutation()
+    const navigate = useNavigate()
+    // console.log(user)
+
+    // dropdown menus
+    const [isDropDownOpen, setIsDropDownOpen] = useState(false)
+
+    const handleDropDownToggle = () => {
+        setIsDropDownOpen(!isDropDownOpen)
+    }
+
+    // admin dropdown menus
+    const adminDropDownMenus = [
+        {label: 'Dashboard', path: '/dashboard/admin'},
+        {label: 'Manage Items', path: '/dashboard/manage-products'},
+        {label: 'All Orders', path: '/dashboard/manage-orders'},
+        {label: 'Add New Post', path: '/dashboard/add-new-post'},
+    ]
+
+    // user dropDown menus
+    const userDropDownMenus = [
+        {label: 'Dashboard', path: '/dashboard'},
+        {label: 'Profile', path: '/dashboard/profile'},
+        {label: 'Payments', path: '/dashboard/payments'},
+        {label: 'Orders', path: '/dashboard/orders'},
+    ]
+
+    const dropDownMenus = user?.role === 'admin' ? [...adminDropDownMenus] : [...userDropDownMenus]
+
+    const handleLogout = async () => {
+        try {
+            await logoutUser().unwrap()
+            dispatch(logout())
+            navigate('/')
+        } catch (error) {
+            console.error("Failed At Logged out, ", error)
+        }
+    }
+
 
     return (
         <header className="fixed-nav-bar w-nav">
@@ -45,17 +91,43 @@ const Navbar = () => {
                         </button>
                     </span>
                     <span>
-                        <Link to="/login">
-                            <i className="ri-user-line"></i>
-                        </Link>
+                        {
+                            user && user ? (
+                                <>
+                                <img onClick={handleDropDownToggle} src={user?.profileImage || avatarImg} alt="" className="size-6 rounded-full cursor-pointer" />
+
+                                {
+                                        isDropDownOpen && (
+                                            <div className="absolute right-0 mt-3 p-4 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-50">
+                                                <ul className="font-medium space-y-4 p-2">
+                                                    {
+                                                        dropDownMenus.map((menu, index) => (
+                                                            <li key={index}>
+                                                                <Link onClick={() => setIsDropDownOpen(false)} className="dropdown-items" to={menu.path}>{menu.label}</Link>
+                                                            </li>
+                                                        ))
+                                                    }
+                                                    <li><Link onClick={handleLogout} className="dropdown-items">Logout</Link></li>
+                                                </ul>
+                                            </div>
+                                    )
+                                }
+                                </>
+                            ) : (
+                                <Link to="/login">
+                                    <i className="ri-user-line"></i>
+                                </Link>
+                            )
+                        }
+
                     </span>
                 </div>
-            </nav>
+            </nav >
 
             {
                 isCartOpen && <CartModal products={products} isOpen={isCartOpen} onClose={handleCartToggle} />
             }
-        </header>
+        </header >
     )
 }
 
