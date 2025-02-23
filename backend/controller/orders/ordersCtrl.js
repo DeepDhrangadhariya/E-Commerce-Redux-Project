38c3c017a2eb07a44babc7065a45e860afb048e4
DeepDhrangadhariya <deepgajjar008@gmail.com>
2025-02-23 16:38:14 +0530

    get orders by email route | get order by id route | get all orders route | update order status route | delete order route

--- backend/controller/orders/ordersCtrl.js
+++ backend/controller/orders/ordersCtrl.js
@@ -63,10 +63,121 @@ module.exports.confirmPayment = async (req, res) => {
         }
 
         await order.save()
-        res.json({order})
+        res.json({ order })
 
     } catch (error) {
         console.error("Error Confirming Payment, ", error)
         res.status(500).json({ message: "Failed To Confirm Payment" })
+    }
+}
+
+module.exports.getOrderByEmail = async (req, res) => {
+    const email = req.params.email
+    if (!email) {
+        return res.status(400).send({ message: "Email Is Required" })
+    }
+
+    try {
+
+        const orders = await orderModel.find({ email: email })
+
+        if (orders.length === 0 || !orders) {
+            return res.status(400).send({ message: "No Orders Found For This Email", orders: 0 })
+        }
+
+        res.status(200).send({ orders })
+
+    } catch (error) {
+        console.error("Error Fetching Orders By Email, ", error)
+        res.status(500).send({ message: "Faild To Fetch Orders By Email" })
+    }
+}
+
+module.exports.getOrderById = async (req, res) => {
+    try {
+
+        const order = await orderModel.findById(req.params.id)
+        if (!order) {
+            return res.status(404).send({ message: "Order Not Found" })
+        }
+
+        res.status(200).send(order)
+
+    } catch (error) {
+        console.error("Error Fetching Orders By Id, ", error)
+        res.status(500).send({ message: "Failed To Fetch Orders By Id" })
+    }
+}
+
+module.exports.getAllOrders = async (req, res) => {
+    try {
+
+        const orders = await orderModel.find().sort({ createdAt: -1 })
+        if (orders.length === 0 || !orders) {
+            return res.status(404).send({ message: "No Orders Found", orders: [] })
+        }
+
+        res.status(200).send(orders)
+
+    } catch (error) {
+        console.error("Error Fetching All Orders, ", error)
+        res.status(500).send({ message: "Failed To Fetch All Orders" })
+    }
+}
+
+module.exports.updateOrderStatus = async (req, res) => {
+    const { id } = req.params
+    if (!id) {
+        return res.status(404).send({ message: "Id Is Required" })
+    }
+
+    const { status } = req.body
+    if (!status) {
+        return res.status(400).send({ message: "Status Is Required" })
+    }
+
+    try {
+
+        const updatedOrder = await orderModel.findByIdAndUpdate(id, {
+            status,
+            updatedAt: new Date()
+        }, {
+            new: true,
+            runValidators: true
+        })
+
+        if (!updatedOrder) {
+            return res.status(404).send({ message: "Order Not Found" })
+        }
+
+        res.status(200).json({ message: "Order Status Updated Successfully", order: updatedOrder })
+
+    } catch (error) {
+        console.error("Error Updating Order Status, ", error)
+        res.status(500).send({ message: "Failed To Update Order Status" })
+    }
+}
+
+module.exports.deleteOrder = async (req, res) => {
+    const {id} = req.params
+    if (!id) {
+        return res.status(404).send({message: "Id Is Required"})
+    }
+
+    try {
+        
+        const deletedOrders = await orderModel.findByIdAndDelete(id)
+        if (!deletedOrders) {
+            return res.status(404).send({message: "Order Not Found"})
+        }
+
+        res.status(200).json({
+            message: "Order Deleted Successfully",
+            order: deletedOrders
+        })
+
+    } catch (error) {
+        console.error("Error Deleting Order, ", error)
+        res.status(500).send({message: "Failed To Delete Order"})
     }
 }
--- backend/routes/orders/ordersRoutes.js
+++ backend/routes/orders/ordersRoutes.js
@@ -2,7 +2,15 @@ const express = require('express')
 const routes = express.Router()
 const ordersCtrl = require('../../controller/orders/ordersCtrl')
 
+routes.get('/:email', ordersCtrl.getOrderByEmail)
+routes.get('/order/:id', ordersCtrl.getOrderById)
+routes.get('/', ordersCtrl.getAllOrders)
+
 routes.post('/create-checkout-session', ordersCtrl.createCheckoutSession)
 routes.post('/confirm-payment', ordersCtrl.confirmPayment)
+
+routes.patch('/update-order-status/:id', ordersCtrl.updateOrderStatus)
+
+routes.delete('/delete-order/:id', ordersCtrl.deleteOrder)
 
 module.exports = routes
